d2edb751b01af5cb9059d3893d77abed27cac16c
Artur Poffo <arturpoffoc@hotmail.com>
2022-08-17 14:50:31 -0300

    heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 const linkRoutes = require("./routes/linkRouter")
 const path = require("path")
 
