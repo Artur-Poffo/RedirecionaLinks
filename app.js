37f98f2f5a8f240c5caf564c0679a512b2d7a3a6
Artur Poffo <arturpoffoc@hotmail.com>
2022-08-18 10:25:23 -0300

    resolvendo bug heroku

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 const linkRoutes = require("./routes/linkRouter")
 const path = require("path")
@@ -13,10 +13,10 @@ const options = {
   family: 4, // Use IPv4, skip trying IPv6
 };
 
-// mongoose.connect("mongodb://localhost/Links", options)
-// .then(db => {
-//     console.log("Mongodb OK")
-// }).catch(err => console.log(err))
+mongoose.connect("mongodb://localhost/Links", options)
+.then(db => {
+    console.log("Mongodb OK")
+}).catch(err => console.log(err))
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
